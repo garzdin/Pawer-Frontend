6dae1f37d2c05a6fa127017d6efdd1507ceafc1e
Teodor Garzdin <teodorgarzedin@gmail.com>
2019-01-17 21:22:26 +0200

    Add more fields, when creating a user. Add methods for updating a user’s first and last names.

--- src/services/parse.js
+++ src/services/parse.js
@@ -12,6 +12,12 @@ import {
   updateEmailRequest,
   updateEmailSuccess,
   updateEmailFailure,
+  updateFirstNameRequest,
+  updateFirstNameSuccess,
+  updateFirstNameFailure,
+  updateLastNameRequest,
+  updateLastNameSuccess,
+  updateLastNameFailure,
 } from '../actions';
 
 Parse.serverURL = 'http://localhost:1337/parse';
@@ -19,14 +25,30 @@ Parse.initialize('CVbIS8GKIo95f5wMwluF6DbjxvjFKL465QwCsJOl', '6PZBT19DLKB7Z1QP5Y
 
 export const currentUser = Parse.User.current();
 
-export function signUp(email, password) {
+export function signUp(email, password, options) {
   return async (dispatch) => {
     dispatch(signUpRequest());
 
+    const {
+      firstName,
+      lastName,
+      address,
+      city,
+      postalCode,
+      country,
+    } = options;
+
     const user = new Parse.User();
+
     user.set('username', email);
     user.set('password', password);
     user.set('email', email);
+    user.set('firstName', firstName);
+    user.set('lastName', lastName);
+    user.set('address', address);
+    user.set('city', city);
+    user.set('postalCode', postalCode);
+    user.set('country', country);
 
     try {
       await user.signUp();
@@ -78,6 +100,46 @@ export function updateEmail(user, email) {
       }
     } else {
       dispatch(updateEmailFailure({ message: 'Couldn\'t update email.' }));
+    }
+  };
+}
+
+export function updateFirstName(user, firstName) {
+  return async (dispatch) => {
+    dispatch(updateFirstNameRequest());
+
+    const result = user.set('firstName', firstName);
+
+    if (result) {
+      try {
+        await user.save();
+
+        dispatch(updateFirstNameSuccess(user));
+      } catch (error) {
+        dispatch(updateFirstNameFailure(error));
+      }
+    } else {
+      dispatch(updateFirstNameFailure({ message: 'Couldn\'t update first name.' }));
+    }
+  };
+}
+
+export function updateLastName(user, lastName) {
+  return async (dispatch) => {
+    dispatch(updateLastNameRequest());
+
+    const result = user.set('lastName', lastName);
+
+    if (result) {
+      try {
+        await user.save();
+
+        dispatch(updateLastNameSuccess(user));
+      } catch (error) {
+        dispatch(updateLastNameFailure(error));
+      }
+    } else {
+      dispatch(updateLastNameFailure({ message: 'Couldn\'t update last name.' }));
     }
   };
 }
