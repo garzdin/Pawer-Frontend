f65229b4c584abc665e9901d285979d6d2e7b432
Teodor Garzdin <teodorgarzedin@gmail.com>
2019-01-20 20:19:05 +0200

    Add a reducer for pets.

---
+++ src/reducers/pets.js
@@ -0,0 +1,11 @@
+const initialState = {
+  pets: [],
+  loading: false,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
