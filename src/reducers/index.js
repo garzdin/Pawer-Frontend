d1b3adba488d64fb3a4713cbec99ceff5be7e45d
Teodor Garzdin <teodorgarzedin@gmail.com>
2019-01-20 20:19:50 +0200

    Add the pets reducer to the reducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import pets from './pets';
 
-export default combineReducers({ auth });
+export default combineReducers({ auth, pets });
