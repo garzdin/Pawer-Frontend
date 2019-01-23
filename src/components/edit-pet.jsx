41fa633688a16647d0a433425b2b3266fa49ba7f
Teodor Garzdin <teodorgarzedin@gmail.com>
2019-01-23 11:48:06 +0200

    Add age and weight to the edit pet form.

--- src/components/edit-pet.jsx
+++ src/components/edit-pet.jsx
@@ -78,6 +78,8 @@ class EditPet extends React.Component {
 
     const name = this.name.value;
     const breed = this.breed.value;
+    const age = this.age.value;
+    const weight = this.weight.value;
     const info = this.info.value;
     const avatar = this.avatar.files.length > 0 ? this.avatar.files[0] : null;
 
@@ -87,6 +89,14 @@ class EditPet extends React.Component {
 
     if (breed) {
       changes.breed = breed;
+    }
+
+    if (age) {
+      changes.age = age;
+    }
+
+    if (weight) {
+      changes.weight = weight;
     }
 
     if (info) {
@@ -130,6 +140,8 @@ class EditPet extends React.Component {
 
     const name = pet.get && pet.get('name');
     const breed = pet.get && pet.get('breed');
+    const age = pet.get && pet.get('age');
+    const weight = pet.get && pet.get('weight');
     const info = pet.get && pet.get('info');
     const avatar = pet.get && pet.get('avatar') && pet.get('avatar').url && pet.get('avatar').url();
 
@@ -176,6 +188,20 @@ class EditPet extends React.Component {
                       </Grid>
                     </Grid>
                     <Typography className={classes.sectionHeader}>Pet Information</Typography>
+                    <Grid container spacing={spacing.unit}>
+                      <Grid item md={6} xs={12}>
+                        <FormControl margin="dense" required fullWidth>
+                          <InputLabel htmlFor="age">Age</InputLabel>
+                          <Input key={age} inputRef={r => this.setRef('age', r)} id="age" name="age" autoComplete="age" defaultValue={age || ''} />
+                        </FormControl>
+                      </Grid>
+                      <Grid item md={6} xs={12}>
+                        <FormControl margin="dense" required fullWidth>
+                          <InputLabel htmlFor="weight">Weight</InputLabel>
+                          <Input key={weight} inputRef={r => this.setRef('weight', r)} id="weight" name="weight" autoComplete="weight" defaultValue={weight || ''} />
+                        </FormControl>
+                      </Grid>
+                    </Grid>
                     <FormControl margin="dense" required fullWidth>
                       <InputLabel htmlFor="info">Info</InputLabel>
                       <Input key={info} inputRef={r => this.setRef('info', r)} id="info" name="info" autoComplete="info" defaultValue={info || ''} />
